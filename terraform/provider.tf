baa5fd55ff87184f6bb90d23c9206288326c1543
Tusharsharma08 <66166132+Tusharsharma08@users.noreply.github.com>
2024-12-24 14:44:30 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,12 +4,12 @@ provider "azurerm" {
 
 # Set the Azure Provider source and version being used
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.1.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.1.0"
+      version = "3.94.0"
     }
   }
 }
