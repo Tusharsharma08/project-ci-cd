844e84c7bf6d8d80872a14eef0802b20975ba3a9
Tusharsharma08 <66166132+Tusharsharma08@users.noreply.github.com>
2024-12-24 15:33:10 +0530

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,9 @@
+# Input variable: Name of Resource Group
+variable "resource_group" {
+  description = "The name of the Resource Group"
+}
+
+# Input variable: Name of the Cluster
+variable "cluster_name" {
+  description = "Cluster Name"
+}
